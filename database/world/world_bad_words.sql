b1e5617fa6a0bca3068e6acfeb23899133ba0dd3
Hydrofluoric0 <90143740+Hydrofluoric0@users.noreply.github.com>
2021-09-11 20:50:45 +0200

    database updated

--- database/world/world_bad_words.sql
+++
@@ -1,9 +0,0 @@
-create table bad_words
-(
-    Id   int auto_increment
-        primary key,
-    Text mediumtext not null
-)
-    engine = MyISAM
-    charset = utf8;
-
--- database/world/world_items_dungeons.sql
+++
@@ -1,9 +0,0 @@
-create table items_dungeons
-(
-    Id     int auto_increment
-        primary key,
-    ItemId int not null
-)
-    engine = MyISAM
-    charset = utf8;
-
